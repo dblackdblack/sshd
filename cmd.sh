5398bcdb323c6774b678afd02eef41916d30f307
dblackdblack <github@dhb.is>
2018-07-12 14:12:31 -0700

    Update cmd.sh

--- cmd.sh
+++ cmd.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -xe
-    
-install -o root -g root -m 0400 /secrets/ecdsa-private /etc/ssh/ssh_host_ecdsa_key
-install -o root -g root -m 0444 /secrets/ecdsa-public /etc/ssh/ssh_host_ecdsa_key.pub
-install -o ubuntu -g ubuntu -m 0400 /secrets/user-ssh-keys /home/ubuntu/.ssh/authorized_keys
+
+install -o root -g root -m 0400 /secrets/ssh-secrets/ecdsa-private /etc/ssh/ssh_host_ecdsa_key
+install -o root -g root -m 0444 /secrets/ssh-secrets/ecdsa-public /etc/ssh/ssh_host_ecdsa_key.pub
+install -o ubuntu -g ubuntu -m 0400 /secrets/ssh-secrets/user-ssh-keys /home/ubuntu/.ssh/authorized_keys
 install -o root -g root -m 0600 -d /run/sshd
 
 exec /usr/sbin/sshd -D
